9324025ccfb6b4fac55dcf8c2b67f3899a238288
Bridgette Kirk <Bridgette Kirk>
2017-02-27 19:00:39 -0600

    Updated with new data file from Ricardo.  Continued modifying wrangling. Assigned mesh types to each of the traps.  Started working to add number of days each trap was set out.

--- DataWrangling/SeedRainWrangling.R
+++ DataWrangling/SeedRainWrangling.R
@@ -9,14 +9,18 @@ library(ggplot2)
 #set working directory to folder
 setwd("Data/RawData")
 
+#naming datafile seedrain from excel file and pulling data from sheet 4
+seedrain<-read_excel("Lluvia de semillas_RBA_20Apr15.xlsx", sheet=4, col_names=TRUE, na= "NA")
 
-seedrain<-read_excel("Seed rain_2014-2015_Lluvia de semillas_RBA_20_Abr_15.xlsx", sheet=3, col_names=TRUE, na= "NA")
-
+#shows what type of variable is in which column
 str(seedrain)
+
+#Change col names to english and abbreviate
 colnames(seedrain) <- c("week", "date", "trap", "sample", "canopysp", "block", "quad", "type", "species", "fruitnum", "seednum", "poop", "damaged", "obs")
 
-
+#Check to see that names were changed
 str(seedrain)
+#provides summary values for all columns in seed rain
 summary(seedrain)
 
 seedrain$species<-as.factor(seedrain$species)
@@ -32,6 +36,52 @@ levels(seedrain$species)<-gsub("Werahuia gladioflora", "Werauhia gladioliflora",
 #check to see what species names are now
 levels(seedrain$species)
 
+#Still working to get this correct where we can add additional date column to figure out the number of days the trap was set out for.
+for (i in 1:nrow(seedrain)){
+  seedrain$date[i] <- gsub(" UTC", "", seedrain$date[i])
+}
+names(seedrain)
+#Get unique rows of trap and date
+seed_rain_unique <- ddply(seedrain, .(date, trap), summarise, n=length(trap))
+
+#Sort by trap and date
+seed_rain_unique <- seed_rain_unique[order(seed_rain_unique$trap, seed_rain_unique$date),]
+#Create days variable
+seed_rain_unique$days <- NA
+
+for (i in 1:{nrow(seed_rain_unique)-1}){
+  seed_rain_unique$days[{i+1}] <- seed_rain_unique$date[{i+1}]-seed_rain_unique$date[i]
+}
+
+seed_rain_unique$date <- as.character(seed_rain_unique$date)
+seed_rain_unique$days[seed_rain_unique$date=="2014-01-20"] <- 7
+seed_rain_unique$days[seed_rain_unique$date=="2014-01-21"] <- 8
+
+
+seedrain$meshtype <- NA
+
+#Create list of trap numbers
+trap_numbers <- seq(1:75)
+#Create list of numbers that are meshsmall
+meshsmall <- c(1, 2, 4, 7, 8, 9, 11, 12, 15, 16, 18, 19, 21, 23, 24, 27, 29, 30, 32, 34, 35, 36, 37, 38, 41, 42, 43, 46, 47, 49, 51, 52, 53, 57, 58, 59, 62, 64, 65, 66, 67, 69, 72, 73, 75)
+
+meshreg <- trap_numbers[-which(trap_numbers%in%meshsmall)]
+meshreg
+
+#Assign Meshsmall to traps that are mesh small
+seedrain$meshtype[seedrain$trap %in% meshsmall] <- "meshsmall"
+seedrain$meshtype[seedrain$trap %in% meshreg] <- "meshreg"
+
+#Create data set with just Mesh1 traps
+seed_rain_unique_meshsmall <- seedrain[seedrain$meshtype=="meshsmall",]
+
+
+
+
+#remove rows of data on unnamed species (no longer exist in new file from Ricardo)
+seedrain2<-seedrain[seedrain$species!="Muestra 100",]  #this is not working yet.
+
+#Once finished wrangling save data in a tidy file
 setwd("../")
 setwd("TidyData")
 write.csv(seedrain,"seedraintidy.csv")
@@ -40,7 +90,5 @@ write.csv(seedrain,"seedraintidy.csv")
 
 
 
-#Clean up data to look at species across the four plots removing data that is irrelevant
 
-#remove rows of data on unnamed species
-seedrain2<-seedrain[seedrain$species!="Muestra 100",]  #this is not working yet.
+
