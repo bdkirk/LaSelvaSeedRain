3356e3e41da838b16eed5283c4951d01c17561dd
Bridgette Kirk <Bridgette Kirk>
2017-04-06 22:27:10 -0500

    Edited based on new names of datasets

--- Analysis/seedrainanalysis.R
+++ Analysis/seedrainanalysis.R
@@ -9,69 +9,58 @@ library(vegan)
 setwd("~/M.S. Thesis/Data/GitHubProjects/LaSelvaSeedRain/Data/TidyData")
 #bring in NMDS data
 seedrain<-read.csv("species_data.csv")
-str(seedrain[,140:144])
 #This gives the number of columns
 ncol(seedrain)
+#This shows what those columns will look like
+str(seedrain[,1:10])
 
-##Already had canopysp in the dataset with block
-#bring in another dataset to match
-#seedrain2 <- read.csv("seedrain_alltidy.csv")
 
-#This adds in canopy species by matching them with the trap from the other data
-#original$column <- new$column[match(original$column, new$column)]
-#seedrain$canopysp <- seedrain2$canopysp[match(seedrain$trap, seedrain2$trap)]
-#str(seedrain[,140:144])
-#Makes a data frame for the sum of each row which will then be added as a column in the dataset
-seedabundance <- as.data.frame(rowSums(seedrain[,-c(1:4,143,144)]))
+#This gives abundance per trap [may need to change so this is abundance per canopy sp]
+seedabundance <- as.data.frame(rowSums(seedrain[,-c(1:6)]))
 str(seedabundance)
 View(seedabundance)
 
-#Creates a dataframe of diversity of all species and excludes the grouping factors or rows
-seeddiversity <- as.data.frame(diversity(seedrain[,-c(1:4,143,144)], index="shannon"))
+#This gives diversity per trap [need to change so this is diversity per canpy sp]
+seeddiversity <- as.data.frame(diversity(seedrain[,-c(1:6)], index="shannon"))
 #check to see what these look like
 str(seeddiversity)
 View(seeddiversity)
 
 #species richness analysis
-seedrich <- as.data.frame(specnumber(seedrain[,-c(1:4,143,144)]))
+seedrich <- as.data.frame(specnumber(seedrain[,-c(1:6)]))
+View(seedrich)
+
 #If you have margin=2 then it finds the species frequencies
+seedrain2 <- seedrain
 
-#bind these columns into one dataset because they have the same number of rows
-seedrain3 <- cbind(seedrain, seedabundance, seeddiversity, seedrich)
-str(seedrain3[,141:147])
-
-#This is not necessary as part of the analysis but keeps the dataset available for future use/tweaking
-#Create a new object based off the original object
-#seedrain4 <- seedrain
-
-#rename the columns with more meaningful names
-seedrain4$Abundance <- seedabundance$`rowSums(seedrain[, -c(1:4, 143, 144)])`
-seedrain4$Diversity <- seeddiversity$`diversity(seedrain[, -c(1:4, 143, 144)], index = "shannon")`
-seedrain4$Richness <- seedrich$`specnumber(seedrain[, -c(1:4, 143, 144)])`
-str(seedrain4[,141:147])
+#rename the columns with more meaningful names and  put them in the other dataset
+seedrain2$Abundance <- seedabundance$`rowSums(seedrain[, -c(1:6)])`
+seedrain2$Diversity <- seeddiversity$`diversity(seedrain[, -c(1:6)], index = "shannon")`
+seedrain2$Richness <- seedrich$`specnumber(seedrain[, -c(1:6)])`
+str(seedrain2[,141:147])
 
 #write as a csv
-write.csv(seedrain4, "div_ab_rich.csv")
+write.csv(seedrain2, "div_ab_rich.csv")
 
 #aov is an anova calculation of the diversity across the four canopy species
-summary(aov(Diversity ~ canopysp, data = seedrain4))
+summary(aov(Diversity ~ canopysp, data = seedrain2))
 
 ###This is not the appropriate analysis but it does show significance between treatment and diversity
-pairwise.t.test(seedrain4$Diversity, seedrain4$canopysp)
+pairwise.t.test(seedrain3$Diversity, seedrain2$canopysp)
 
 
 ###FIGURE OUT HOW TO PLOT WITH TWO DIFFERENT VARIABLES i.e.canopy and block where it is averaged within the canopy for the block
 #plot of diversity
 library(ggplot2)
-ggplot(seedrain4, aes(canopysp, Diversity))+
+ggplot(seedrain2, aes(canopysp, Diversity))+
   geom_violin()
 
 #plot of abundance
-ggplot(seedrain4, aes(canopysp, Abundance))+
+ggplot(seedrain2, aes(canopysp, Abundance))+
   geom_boxplot()
 
 #plot of richness
-ggplot(seedrain4, aes(canopysp, Richness))+
+ggplot(seedrain2, aes(canopysp, Richness))+
   geom_violin()
 
 
