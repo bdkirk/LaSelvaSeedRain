cd5beef7664cf34f896e77d40b51e40f47c6d298
Bridgette Kirk <Bridgette Kirk>
2017-04-06 22:26:35 -0500

    Finished fixing data so that it looked at the sum total by trap for NMDS calculations

--- Analysis/PERMANOVA_NMDS.R
+++ Analysis/PERMANOVA_NMDS.R
@@ -6,14 +6,6 @@ library(ggplot2); library(vegan); library(geomorph)
 #set wd
 setwd("~/M.S. Thesis/Data/GitHubProjects/LaSelvaSeedRain/Data/TidyData")
 
-#Get data
-speciesdata <- read.csv("div_ab_rich.csv")
-#This line of code restricts the data analyzed to having a species number greater than 5; Confirm the biology makes this a good number.
-speciesdata2 <- subset(speciesdata, specnumber(speciesdata[,6:143]) >= 5)
-#This eliminates all the factors so that they are not used in parts of the analysis
-seedspecies <- speciesdata2[,6:143]
-
-
 ##  --------------------------------------------------------------------------------------------------------------------------------------  ##
 # Function for Plotting NMS results (Nick Lyon's code)
 ##  --------------------------------------------------------------------------------------------------------------------------------------  ##
@@ -39,81 +31,175 @@ nmsplot <- function(mod, groupcol, g1, g2, g3, g4, legpos, legcont) {
   ordiellipse(mod, groupcol, col = c("#006600", "#FF6600", "#990066", "#0066CC"), display = "sites", kind = "sd", label = F)
   
   # Add legend
-  legend(legpos, legend = legcont, fill = c("#006600", "#FF6600", "#990066", "#0066CC"))
+  legend(legpos, legend = legcont, fill = c("#006600", "#FF6600", "#990066", "#0066CC"), cex = 0.75)
   
 }
 
 #############Seedrain Analysis for All data####
 # Using Bray-Curtis dissimilarity index
+#Get data
+#speciesdata <- read.csv("species_data.csv")
+#ncol(speciesdata)
+#str(speciesdata[,1:10])
+#This line of code restricts the data analyzed to having a species number greater than 5; Confirm the biology makes this a good number.
+#speciesdata2 <- subset(speciesdata, specnumber(speciesdata[,7:144]) >= 1)
+
+#speciesdata$delete <- 0
+#for (i in 1:nrow(speciesdata)){
+ # speciesdata$delete[i] <- sum(speciesdata[i,6:143]>0)
+#}
+
+#subsetspecies_data <- speciesdata[speciesdata$delete>=5,]
+#This eliminates all the factors so that they are not used in parts of the analysis
+#seedspecies <- speciesdata2[,6:143]
+
 #name new object and do the vegdist (part of vegan), this computes dissimilarity indices to use in the PERMANOVA
-seedrain.bc <- vegdist(seedspecies)
+#seedrain.bc <- vegdist(seedspecies)
 
 # PERMANOVA (Anderson et al. 2001)
-procD.lm(seedrain.bc ~ canopysp*block, data = speciesdata2) # significant diff btw overstory plots
-#This does a pair-wise comparison of the data
-advanced.procD.lm(seedrain.bc ~ canopysp, ~ 1, ~ canopysp, data = speciesdata2) # Four overstory treatments compared with one another
+#procD.lm(seedrain.bc ~ canopysp*block, data = speciesdata2) # significant diff btw overstory plots
 
+#This does a pair-wise comparison of the data
+#advanced.procD.lm(seedrain.bc ~ canopysp, ~ 1, ~ canopysp, data = speciesdata2) # Four overstory treatments compared with one another
+
+#NMDS
+#seedspecies.mds <- metaMDS(seedspecies, autotransform = F, expand = F, k = 2, try = 20)
+#seedspecies.mds$stress
+
+#Ordination
+#nmsplot(seedspecies.mds, speciesdata2$canopysp, "Hial", "Vogu", "Pema", "Viko",
+       # "bottomleft", c("HIAL", "VOGU", "PEMA", "VIKO"))
+
+#I am not sure what this does.
+#stressplot(seedspecies.mds)
+
+
+#####NMDS for all data####
+# Using Bray-Curtis dissimilarity index
+#Get data
+speciesdata <- read.csv("species_data.csv")
+ncol(speciesdata)
+str(speciesdata[,1:10])
+
+seedspecies <- speciesdata[,7:143]
+#name new object and do the vegdist (part of vegan), this computes dissimilarity indices to use in the PERMANOVA
+seedrain.bc <- vegdist(seedspecies)
+# PERMANOVA (Anderson et al. 2001)
+procD.lm(seedrain.bc ~ canopysp*block, data = speciesdata) # significant diff btw overstory plots
+#This does a pair-wise comparison of the data
+advanced.procD.lm(seedrain.bc ~ canopysp, ~ 1, ~ canopysp, data = speciesdata) # Four overstory treatments compared with one another
 #NMDS
 seedspecies.mds <- metaMDS(seedspecies, autotransform = F, expand = F, k = 2, try = 20)
 seedspecies.mds$stress
-
 #Ordination
-nmsplot(seedspecies.mds, speciesdata2$canopysp, "Hial", "Vogu", "Pema", "Viko",
+nmsplot(seedspecies.mds, speciesdata$canopysp, "Hial", "Vogu", "Pema", "Viko",
         "bottomleft", c("HIAL", "VOGU", "PEMA", "VIKO"))
-
 #I am not sure what this does.
 stressplot(seedspecies.mds)
 
 
-
-
-
-
-###Now you will do the same thing analyzing effect of MESH TYPE###############################
+#####All data with no overstory species included###############################
 #Bring data in
-speciesdata_allMesh <- read.csv("div_ab_rich.csv")
-seedrain<- read.csv("seedrain_alltidy.csv")
-
-#original$column <- new$column[match(original$column, new$column)]
-speciesdata_allMesh$meshtype <- seedrain$meshtype[match(speciesdata_allMesh$trap, seedrain$trap)]
-#Check to see if column was added
-str(speciesdata_allMesh[,140:149])
-str(speciesdata_allMesh[,1:10])
+ovsty_rem_nmds <- read.csv("NMDS_ovsty_rem.csv")
+View(ovsty_rem_nmds)
+ncol(ovsty_rem_nmds)
+str(ovsty_rem_nmds[,1:10])
 #Creates a subset for species greater than 5
-sd_allmesh_sub <- subset(speciesdata_allMesh, specnumber(speciesdata_allMesh[,6:142]) >=5)
+#ovsty_rem_sub <- subset(ovsty_rem_nmds, specnumber(ovsty_rem_nmds[,5:137]) >=5)
 #Creates this subset
-seed_mesh_all <- sd_allmesh_sub[,6:142]
+ovsty_rem_sub <- ovsty_rem_nmds[,7:139]
 
-##You will now use speciesdata_allMesh for with the factors and seed_mesh_all for without the factors
-##This begins the analysis
 # Using Bray-Curtis dissimilarity index #name new object and do the vegdist (part of vegan) for the data with no factors, this computes dissimilarity indices to use in the PERMANOVA
-seed_mesh_all.bc <- vegdist(seed_mesh_all)
+ovsty_rem.bc <- vegdist(ovsty_rem_sub)
 
 # PERMANOVA (Anderson et al. 2001)
 #Use data from dissimilarity and can compare treatments 
 ###THIS code will not work because meshtype does not have more than two factos so it cannot be contrasted.
 #procD.lm(seed_mesh_all.bc ~ canopysp + meshtype, data = sd_allmesh_sub) # treatments- canopy is statistically significant, meshtype is not statistically signficant
-procD.lm(seed_mesh_all.bc ~ canopysp + block, data = sd_allmesh_sub)
+procD.lm(ovsty_rem.bc ~ canopysp + block + meshtype + trap, data = ovsty_rem_nmds)
 
 #This does a pair-wise comparison of the data
-advanced.procD.lm(seed_mesh_all.bc ~ canopysp+meshtype, ~canopysp, ~meshtype, data = sd_allmesh_sub) # This shows that there isn't an interaction effect between canopy sp and meshtype
+advanced.procD.lm(ovsty_rem.bc ~ canopysp, ~ 1, ~ canopysp, data = ovsty_rem_nmds) # This shows that there isn't an interaction effect between canopy sp and meshtype
 
 #NMDS
 #new object <- metaMDS(nofactorfile, auto blah blah....)
-meshtype.mds <- metaMDS((seed_mesh_all), autotransform = F, expand = F, k = 2, try = 20)
-seedspecies.mds$stress
+ovsty_rem.mds <- metaMDS((ovsty_rem_sub), autotransform = F, expand = F, k = 2, try = 20)
+ovsty_rem.mds$stress
 
-#Ordination ####this really doesn't do anything because code is written for 4 objects, not two
+#Ordination 
+nmsplot(ovsty_rem.mds, ovsty_rem_nmds$canopysp, "Hial", "Vogu", "Pema", "Viko",
+        "bottomleft", c("HIAL", "VOGU", "PEMA", "VIKO"))
+stressplot(ovsty_rem.mds)
 
-nmsplot(meshtype.mds, sd_allmesh_sub$meshtype, "meshreg", "meshsmall", 
-        "bottomleft", c("Reg", "Small"))
-stressplot(meshtype.mds)
-
-#-#-#_#3_3_THIS IS WHERE YOU NEED TO START
-######################Look at data with no overstory##########
+######################Small mesh with no overstory##########
 #Get data
-speciesdata <- read.csv("div_ab_rich.csv")
+small_rem_nmds <- read.csv("NMDS_small_rem.csv")
 #This line of code restricts the data analyzed to having a species number greater than 5; Confirm the biology makes this a good number.
-speciesdata2 <- subset(speciesdata, specnumber(speciesdata[,4:139]) >= 5)
-#This eliminates all the factors so that they are not used in parts of the analysis
-seedspecies <- speciesdata2[,4:139]
+#check what the last columns were
+ncol(small_rem_nmds)
+str(small_rem_nmds[,1:10])
+
+#Creates a subset for species greater than 5
+#small_rem_sub <- subset(small_rem_nmds, specnumber(small_rem_nmds[,5:119]) >=5)
+#Creates this subset
+small_rem_sub <- small_rem_nmds[,7:121]
+
+# Using Bray-Curtis dissimilarity index #name new object and do the vegdist (part of vegan) for the data with no factors, this computes dissimilarity indices to use in the PERMANOVA
+small_rem.bc <- vegdist(small_rem_sub)
+
+# PERMANOVA (Anderson et al. 2001)
+#Use data from dissimilarity and can compare treatments 
+###THIS code will not work because meshtype does not have more than two factos so it cannot be contrasted.
+#procD.lm(seed_mesh_all.bc ~ canopysp + meshtype, data = sd_allmesh_sub) # treatments- canopy is statistically significant, meshtype is not statistically signficant
+procD.lm(small_rem.bc ~ canopysp + block, data = small_rem_nmds)
+
+#This does a pair-wise comparison of the data
+advanced.procD.lm(small_rem.bc ~ canopysp, ~ 1, ~ canopysp, data = small_rem_nmds) # This shows that there isn't an interaction effect between canopy sp and meshtype
+
+#NMDS
+#new object <- metaMDS(nofactorfile, auto blah blah....)
+small_rem.mds <- metaMDS((small_rem_sub), autotransform = F, expand = F, k = 2, try = 20)
+small_rem.mds$stress
+
+#Ordination 
+nmsplot(small_rem.mds, small_rem_nmds$canopysp, "Hial", "Vogu", "Pema", "Viko",
+        "bottomleft", c("HIAL", "VOGU", "PEMA", "VIKO"))
+stressplot(small_rem.mds)
+
+##############reg mesh with no overstory#########
+#Get data
+reg_rem_nmds <- read.csv("NMDS_reg_rem.csv")
+View(reg_rem_nmds)
+#This line of code restricts the data analyzed to having a species number greater than 5; Confirm the biology makes this a good number.
+#check what the last columns were
+ncol(reg_rem_nmds)
+str(reg_rem_nmds[,1:10])
+str(reg_rem_nmds[,70:74])
+
+#Creates this subset
+reg_rem_sub <- reg_rem_nmds[,7:74]
+
+levels(reg_rem_sub$species)
+
+###########issue!!!
+# Using Bray-Curtis dissimilarity index #name new object and do the vegdist (part of vegan) for the data with no factors, this computes dissimilarity indices to use in the PERMANOVA
+reg_rem.bc <- vegdist(reg_rem_sub)
+#dim(reg_rem_ssub)
+#length(dimnames(reg_rem_ssub))
+# PERMANOVA (Anderson et al. 2001)
+#Use data from dissimilarity and can compare treatments 
+procD.lm(reg_rem.bc ~ canopysp + block, data = reg_rem_nmds)
+
+#This does a pair-wise comparison of the data
+advanced.procD.lm(reg_rem.bc ~ canopysp, ~ 1, ~ canopysp, data = reg_rem_nmds) # This shows that there isn't an interaction effect between canopy sp and meshtype
+
+#NMDS
+#new object <- metaMDS(nofactorfile, auto blah blah....)
+reg_rem.mds <- metaMDS((reg_rem_sub), autotransform = F, expand = F, k = 2, try = 20)
+reg_rem.mds$stress
+
+#Ordination 
+nmsplot(reg_rem.mds, reg_rem_nmds$canopysp, "Hial", "Vogu", "Pema", "Viko",
+        "bottomleft", c("HIAL", "VOGU", "PEMA", "VIKO"))
+stressplot(reg_rem.mds)
+
