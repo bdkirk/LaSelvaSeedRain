72d15a50ca005c4d6e63b9feecbe470d3d05a994
Bridgette Kirk <Bridgette Kirk>
2017-05-11 11:34:48 -0500

    Created wrangling file for diversity

---
+++ DataWrangling/DiversityWrangling.R
@@ -0,0 +1,5 @@
+#Started on 11 May 2017
+# This involves wrangling of data so that tree effects on species diversity can be found
+# Seed rain diversity: Richness, Shannon-Weiner diversity index, Evenness (Pielou's)
+# There will be 15 total observations with traps summarised up to the plot level.
+#Experimental design: RCBD, C. Total df = 14
