6127ba9b89f6bb4330205bc415e2ffccfd77c287
Bridgette Kirk <Bridgette Kirk>
2017-05-12 10:52:54 -0500

    Started file for wrangling for Mesh question.  Will work on other analyses first.

---
+++ DataWrangling/MeshWrangling.R
@@ -0,0 +1,6 @@
+# Started on 12 May 2017
+# This is the wrangling file for the second analysis looking at whether there are differences in seed rain abundance, diversity, composition and phenology across the two mesh sizes
+# This data will be analyzed as a split plot design.  Whole- Block (4); whole factor- trt group/overstory spp (4); Subplot/splitplot- plot (15); subplot/splitplot factor- mesh(2)
+
+# This will require a summarized long format tidy file for abundance; a wide format tidy file for diversity and composition; pheno?? #not sure of pheno yet, will find out after meeting with stat help
+
