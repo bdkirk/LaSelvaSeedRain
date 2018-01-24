e5dbcb5cfcff6c502e469b5e8e4b1a9e8c94f8eb
Bridgette Kirk <bdaniellekirk@gmail.com>
2018-01-24 12:14:12 -0600

    reran code to extract analysis.  Also cleaned up code.

--- Analysis/Abundance_Analysis.R
+++ Analysis/Abundance_Analysis.R
@@ -6,7 +6,7 @@
 ################### Abundance Analysis ##########
 
 #Load libraries
-library(ggplot2); library(car); library(lsmeans); library(stats); library(lme4)
+library(ggplot2); library(car); library(lsmeans); library(stats); library(lme4); library(dplyr)
 
 #Bring in data
 setwd("~/M.S. Thesis/Data/GitHubProjects/LaSelvaSeedRain/Data/TidyData")
@@ -27,6 +27,11 @@ str(abund)
 abund$Dist <- as.factor(abund$Dist)
 abund$ï..Plot <- NULL
 
+
+######## Exploration of Data #######
+#This section looks at how distance, slope and aspect vary in relation to seed size.
+
+
 #Plotting to look at how the data relates
 plot(abund$total_seednum, abund$Slope)
 plot(abund$total_seednum, abund$Aspect)
@@ -35,41 +40,40 @@ plot(abund$total_seednum, abund$Dist)
 
 
 #add in se and mean to dataset
-library(dplyr)
 abund_summary <- abundanalysis %>% # the names of the new data frame and the data frame to be summarised
-  group_by(canopysp) %>%   # the grouping variable
+  group_by(treatment) %>%   # the grouping variable
   summarise(mean_PL = mean(total_seednum),  # calculates the mean of each group
             sd_PL = sd(total_seednum), # calculates the standard deviation of each group
             n_PL = n(),  # calculates the sample size per group
             SE_PL = sd(total_seednum)/sqrt(n())) # calculates the standard error of each group
   
 #make plot
-library(ggplot2)
-#ggplot(abund_summary, aes(canopysp, mean_PL, fill= canopysp))+
-  #geom_boxplot()+
+#below does not work.
+#ggplot(abund_summary, aes(treatment, mean_PL, fill= treatment))+
+ # geom_boxplot()+
   #ggtitle("Total Abundance Across Treatments")+
   #geom_errorbar(abund_summary, aes(ymin=mean_PL-SE_PL, ymax=mean_PL+SE_PL), width=0.2)+
-  #xlab("Planted Tree Treatments")+
+  #xlab("Planted Tree Species")+
   #ylab("Abundance")+
   #guides(fill=FALSE)
 
 #looking for trend between distance from forest and total seed num across treatments, none found.
-ggplot(abund, aes (Slope, total_seednum))+
-  geom_point(aes(color = canopysp))
+ggplot(abund, aes (Dist, total_seednum))+
+  geom_point(aes(color = treatment))
 
 #Log transform the data because the numbers are very large. Don't really need to do this step
 #decided not to logtransform the data because it is count data that will have overdispersion that will be taken into account with 
 #abundanalysis$logsum <- log(abundanalysis$total_seednum)
 #str(abundanalysis)
 #abundanalysis$block <- as.factor(abundanalysis$block)
-#abundanalysis$canopysp <- as.factor(abundanalysis$canopysp)
+#abundanalysis$treatment <- as.factor(abundanalysis$treatment)
 
-########### Data Exploration #########
+########### Data Exploration for specific analysis#########
 ##a.  Outliers in Y / Outliers in X (Step 1)
 #i.	plot response and predictors to check for outliers  (only with continuous data)
 #1.	Use Mydotplot or dotplot or boxplot, identify outliers
 hist(abundanalysis$total_seednum)
-boxplot(abundanalysis$total_seednum~ abundanalysis$canopysp, xlab="treatment", ylab="seednum", main= "Seed abundance per treatment")
+boxplot(abundanalysis$total_seednum~ abundanalysis$treatment, xlab="treatment", ylab="seednum", main= "Seed abundance per treatment")
 
 
 #look at distance
@@ -77,7 +81,7 @@ boxplot(abund$total_seednum~abund$Dist)
 #no clear trend seen here
 
 #No difference found when looking at logsum of abundance.
-#boxplot(abundanalysis$logsum~ abundanalysis$canopysp, xlab="canopysp", ylab="seednum")
+#boxplot(abundanalysis$logsum~ abundanalysis$treatment, xlab="treatment", ylab="seednum")
 
 #Much more variance in hial
 #one outlier in Y, hial 2 but will keep, X is categorical
@@ -87,27 +91,27 @@ boxplot(abund$total_seednum~abund$Dist)
 
 ##c.	Collinearity X: correlation between covariates (Step 5)
 #i.	Plot each predictor against each other (since categorical, will use table to make sure we have all combinations)
-with(abundanalysis, table(canopysp, total_seednum))
+with(abundanalysis, table(treatment, total_seednum))
 with(abund, table(Dist, total_seednum))
 
 #Missing one plot, vogu1.
 ##good = balanced, bad = unequal sample sizes, ugly = one or more combination is missing
 #check variance inflation factor
-vif(glm(total_seednum~ canopysp +block + Dist, data = abund))
-vif(glm(total_seednum~canopysp +Dist, data = abund))
-# model shows interactions when include the canopysp*block, shows collinearity
+vif(glm(total_seednum~ treatment +block + Dist, data = abund))
+vif(glm(total_seednum~treatment +Dist, data = abund))
+# model shows interactions when include the treatment*block, shows collinearity
 #vif was less than 10
 
 #d.	Homogeneity of variance? (Step 2)
 #Look at relationships of Y vs X’s to see if homogenous variances at each X value, linear relationships
 # i.	Plot response against each predictor and random effect. 
-ggplot(abundanalysis, aes(canopysp, total_seednum, color=block))+
+ggplot(abundanalysis, aes(treatment, total_seednum, color=block))+
   geom_boxplot()
 #more variance with Hial, minimal variance with vogu. pema and viko have a similar variance.
 
 #e.	Independence Y - are Y's independent? (Step 8)
 #1.	Is there a pattern across time or space that is not incorporated in the model? 
-ggplot(abundanalysis, aes(canopysp, total_seednum, color=canopysp))+
+ggplot(abundanalysis, aes(treatment, total_seednum, color=treatment))+
   geom_boxplot()+
   facet_grid(.~block)
 
@@ -115,10 +119,11 @@ ggplot(abundanalysis, aes(canopysp, total_seednum, color=canopysp))+
 ggplot(abund, aes(Dist, total_seednum))+
   geom_point()
 
-hist(abund$Dist)
-boxplot(abund$Dist ~ abund$total_seednum)
+#Characters are not correct for the following two lines
+#hist(abund$Dist)
+#boxplot(abund$Dist ~ abund$total_seednum)
 
-with(abund, ftable(canopysp, Dist))
+with(abund, ftable(treatment, Dist))
 #This table is not really useful in this case
 
 #More variance with block 2 and more variance with Hial 
@@ -132,13 +137,13 @@ with(abund, ftable(canopysp, Dist))
 
 #i.	Do all levels of Island have adequate samples of at each level of Native? 	Examine interactions
 #Is the quality of the data good enough to include them? (i.e. do we have enough samples for each level of the interaction?) 
-with(abundanalysis, table(block, canopysp))
+with(abundanalysis, table(block, treatment))
 
 ###Katie Rey Data Visualization####
 # This is an alternative way of exploring the data.
-library(dplyr); library(ggplot2)
+#library(dplyr); library(ggplot2)
 #Find outliers
-ggplot(abundanalysis, aes(canopysp, total_seednum))+
+ggplot(abundanalysis, aes(treatment, total_seednum))+
   geom_point()
 #Appears to be an outlier for hial2
 
@@ -148,7 +153,7 @@ cbPalette <- c("#999999", "#56B4E9", "#F0E442", "#CC79A7")
 
 abundanalysis %>% 
   filter(!is.na(total_seednum)) %>%
-  ggplot()+geom_bar(aes(canopysp,fill=as.factor(canopysp)))+
+  ggplot()+geom_bar(aes(treatment,fill=as.factor(treatment)))+
   facet_grid(plot~.)+
   scale_fill_manual(values=cbPalette)+
   theme(legend.position="none")
@@ -158,12 +163,12 @@ abundanalysis %>%
 #Non constant variance: helps to identify if all of the treatments have the same variance
 ggplot(abundanalysis, aes(block, total_seednum))+
   geom_boxplot(aes(fill=block))+
-  facet_grid(.~canopysp)+
+  facet_grid(.~treatment)+
   scale_fill_manual(values=cbPalette)+
   theme(legend.position="none")
 #summary
 # block 2 and Hial have more variability than other block and canopsysp treatments
-# can go ahead looking at total seed abundance relative to block and canopysp. 
+# can go ahead looking at total seed abundance relative to block and treatment. 
 
 
 #################################################
@@ -182,9 +187,9 @@ ggplot(abundanalysis, aes(block, total_seednum))+
 #1) Does abundance of seeds differ between overstory treatments? 
 
 #using all data
-#total_seednum~ block+canopysp, family=gaussian  #by default, an identity link
+#total_seednum~ block+treatment, family=gaussian  #by default, an identity link
 
-abundmod1<-glm(total_seednum~block+canopysp, data=abundanalysis)
+abundmod1<-glm(total_seednum~block+treatment, data=abundanalysis)
 summary(abundmod1)
 anova(abundmod1) #should not use if unbalanced
 
@@ -198,7 +203,7 @@ head(model.matrix(abundmod1))
 
 # anova(model) gives Type I sums of squares, which means the reference level is tested first and then other levels, and then interactions. R defaults to treatment contrasts. Can get different results for unbalanced datasets depending on which factor is entered first in the model and thus considered first. 
 
-with(abundanalysis,tapply(total_seednum, list(canopysp, block), mean))
+with(abundanalysis,tapply(total_seednum, list(treatment, block), mean))
 
 ##can use car package to do Type II or III sums of squares
 #Type III can be used with interactions
@@ -207,28 +212,28 @@ Anova(abundmod1, type="III")
 #explore contrasts
 options('contrasts') #shows what contrasts R is using
 #can set contrasts to SAS default. 
-abundmod1a<-glm(total_seednum~block+canopysp, data=, contrasts = list(canopysp = "contr.SAS", block="contr.SAS"))
+abundmod1a<-glm(total_seednum~block+treatment, data=, contrasts = list(treatment = "contr.SAS", block="contr.SAS"))
 summary(abundmod1a)
 
 #Type II
-Anova(glm(total_seednum~block+canopysp, data=abundanalysis), type="II")  #note - type II can't handle interactions
+Anova(glm(total_seednum~block+treatment, data=abundanalysis), type="II")  #note - type II can't handle interactions
 #compare against 
-anova(glm(total_seednum~canopysp+block, data=abundanalysis))
-anova(glm(total_seednum~block+canopysp, data=abundanalysis))
+anova(glm(total_seednum~treatment+block, data=abundanalysis))
+anova(glm(total_seednum~block+treatment, data=abundanalysis))
 
 #lsmeans
-cpyblk<-pairs(lsmeans(abundmod1, ~canopysp | block)) # in lsmeans package
-blkcpy <- pairs(lsmeans(abundmod1, ~block | canopysp))
+cpyblk<-pairs(lsmeans(abundmod1, ~treatment | block)) # in lsmeans package
+blkcpy <- pairs(lsmeans(abundmod1, ~block | treatment))
 rbind(cpyblk, blkcpy)
 
 ####### 2. Classic model selection: Create all sub-models. Use LRT to come up with best model. #####
-abundmod1<-glm(logsum~block+canopysp, data=abundanalysis)
-abundmod2<-glm(total_seednum~canopysp+block, data=abundanalysis)
-abundmod3<-glm(logsum~canopysp, data=abundanalysis)
+abundmod1<-glm(logsum~block+treatment, data=abundanalysis)
+abundmod2<-glm(total_seednum~treatment+block, data=abundanalysis)
+abundmod3<-glm(logsum~treatment, data=abundanalysis)
 abundmod4<-glm(logsum~block, data=abundanalysis)
 abundmod5 <- glm(total_seednum~plot, data=abundanalysis)
 abundmod_null<-glm(total_seednum~1, data=abundanalysis)
-abundmod6 <- glm(logsum~canopysp*block, data=abundanalysis)
+abundmod6 <- glm(logsum~treatment*block, data=abundanalysis)
 
 summary(abundmod5)
 anova(abundmod1, abundmod2, test = "Rao")  #model 1 not sig better than 2
@@ -273,7 +278,7 @@ plot(x = F1,
      cex.lab = 1.5)
 abline(h = 0, lty = 2)
 
-plot(x=abundanalysis$canopysp, y=F1) #heterogeneity in residuals bt canopysp
+plot(x=abundanalysis$treatment, y=F1) #heterogeneity in residuals bt treatment
 plot(x=abundanalysis$block, y=F1) #heterogeneity in residuals wrt Native
 plot(x=abundanalysis$plot, y=F1) #residual variance larger at Guam sites than Saipan sites, but homogeneity bt sites within an island
 
@@ -284,7 +289,7 @@ plot(abundmod1)
 plot(abundmod2) 
 #This part involves determining which model is most appropriate
 #get residuals and plot residuals vs predicted values
-abund.glm <-  glm(total_seednum ~ block+canopysp, data=abundanalysis,  family = poisson) 
+abund.glm <-  glm(total_seednum ~ block+treatment, data=abundanalysis,  family = poisson) 
 abund.res = resid(abund.glm) 
 
 plot(abundanalysis$total_seednum, abund.res, ylab="Residuals", xlab="Seed Abundance", main="Abundance pred by resid") 
@@ -295,10 +300,8 @@ head(abundanalysis)
 length(unique(abundanalysis$plot))
 
 
-
-
 #change model to glmer because this will account for overdispersion with the poisson distribution.
-abund.glm <- glmer(total_seednum ~ block+canopysp+(1|plot), data=abundanalysis, 
+abund.glm <- glmer(total_seednum ~ block+treatment+(1|plot), data=abundanalysis, 
                   family = poisson)
 
 abund.res <- resid(abund.glm)
@@ -312,7 +315,7 @@ summary(abund.glm)
 
 #find Tukeys (HSD) and use for this data
 #if p-value is greater than 0.05, don't need to do HSD
-lsmeans(abund.glm, "canopysp", contr= "pairwise")
+lsmeans(abund.glm, "treatment", contr= "pairwise")
 #These p-values are not t-based p-values that account for df but you can get those by using the code below:
 
 #ptukey(Zscore*sqrt(2), nmeans=4, df=8, lower = F)
@@ -342,12 +345,12 @@ ptukey(2.394*sqrt(2), nmeans= 4, df=8, lower = F)
 #=0.1556
 
 ####Have not figured out how to more efficiently export output and do a conversion.
-temp <- lsmeans(abund.glm, "canopysp", contr= "pairwise")
+temp <- lsmeans(abund.glm, "treatment", contr= "pairwise")
 
 str(abund)
 abund$Dist <- as.factor(abund$Dist)
 #Incorporating distance into the model: None of the below works right now.  Need to reread email from Dr. Dixon on 3 July.
-abunddist.glm <- glmer(total_seednum~canopysp+Dist+(1|plot), data= abund, family = poisson)
+abunddist.glm <- glmer(total_seednum~treatment+Dist+(1|plot), data= abund, family = poisson)
 
 summary(abunddist.glm)
 anova(abunddist.glm)
