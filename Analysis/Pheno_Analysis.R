184ed0f31ad5b97cbe7c06fe915efff54155dc0c
Bridgette Kirk <bdaniellekirk@gmail.com>
2018-01-25 13:58:41 -0600

    Updated from canopysp to treatment

--- Analysis/Pheno_Analysis.R
+++ Analysis/Pheno_Analysis.R
@@ -33,14 +33,14 @@ month_plot <- ddply(pheno, .(plot, species, month), summarise, seednum=sum(total
 
 
 #create a monthly seed species, treatment total
-month_treatment <- ddply(pheno, .(canopysp, species, month), summarise, seednum=sum(total_seednum))
+month_treatment <- ddply(pheno, .(treatment, species, month), summarise, seednum=sum(total_seednum))
 
 
 #create a monthly seed species, block total
-month_block <- ddply(pheno, .(block, canopysp, species, month), summarise, seednum=sum(total_seednum))
+month_block <- ddply(pheno, .(block, treatment, species, month), summarise, seednum=sum(total_seednum))
 
 #####using summaries to make graphics######
-
+############################################################################################
 #1)miconia affinis
 miaf <- filter(month_plot, species == "miconia affinis")
 
@@ -55,8 +55,9 @@ ggplot(data = miaf, aes(month, seednum)) +
                geom = "bar")+ # or "line"
   facet_wrap(~plot, scales = "free_y")
 
-
+#########################################################################################
 #2)top ten
+#on 15 August based on recommendations from committee mtgs, changed to log(seednum)
 topspecies <- c("miconia affinis", "piper colonense", "miconia multispicata", "alchorneopsis floribunda", "piper auritifolium", "piptocarpha poeppigiana", "cecropia obtusifolia", "clidemia japurensis", "sabicea villosa", "piper multiplinervium")
                
 topten <- filter(month_plot, species %in% topspecies)
@@ -64,13 +65,15 @@ topten <- filter(month_plot, species %in% topspecies)
 ggplot(data = topten, aes(month, seednum, fill = species)) +
   stat_summary(fun.y = sum, # adds up all observations for the month
                geom = "bar")+ # or "line"
-  scale_x_date(date_breaks = "1 month", labels=date_format ("%b-%Y")) # custom x-axis labels
+  scale_x_date(date_breaks = "1 month", labels=date_format ("%b-%Y"))+ # custom x-axis labels
+  scale_y_log10()
 
 #plots the totals for each plot compared side by side
 ggplot(data = topten, aes(month, seednum)) +
   stat_summary(fun.y = sum, # adds up all observations for the month
                geom = "line")+ # or "line"
-  facet_wrap(~plot, scales = "free_y")
+  facet_wrap(~plot, scales = "free_y")+
+  scale_y_log10()
 
 #try to plot monthly totals for each species within each plot
 ggplot(data = topten, aes(month, seednum), fill= species) +
@@ -85,12 +88,47 @@ ggplot(data = topten_trt, aes(month, seednum, fill = species)) +
   stat_summary(fun.y = sum, # adds up all observations for the month
                geom = "bar")+ # or "line"
   scale_x_date(date_breaks = "1 month", labels=date_format ("%b-%Y"), limits = as.Date(c(NA, NA)))+ # custom x-axis labels
-  facet_grid(canopysp~., scales ="free_y")+
-  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+  facet_grid(treatment~., scales ="free_y")+
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))+
+  scale_y_log10()
+
+#adjust previous plot to use log(seednum) and make the scales similar as requested by committee
+#continue adjusting so that colors are more distinguishable.  Perhaps add a trendline?
+ggplot(data = topten_trt, aes(month, seednum, fill = species)) +
+  stat_summary(fun.y = sum, # adds up all observations for the month
+               geom = "bar")+ # or "line"
+  scale_x_date(date_breaks = "1 month", labels=date_format ("%b-%Y"), limits = as.Date(c(NA, NA)))+ # custom x-axis labels
+  facet_grid(treatment~.)+
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))+
+  scale_y_log10()
 
 #plot top ten species
 ggplot(data = topten_trt, aes(month, seednum, fill = species)) +
   stat_summary(fun.y = sum, # adds up all observations for the month
                geom = "bar")+ # or "line"
   scale_x_date(date_breaks = "1 month", labels=date_format ("%m"))+ # custom x-axis labels
-  facet_grid(canopysp~species)
+  facet_grid(treatment~species)+
+  scale_y_log10()
+#Would be good to get this with the overstory phenology plotted in the background
+
+############################################################################################
+#3 Overstory species
+#a) Plot phenology of overstory species across treatments
+overstory <- c("hieronyma alchorneoides", "pentaclethra macroloba", "virola koschnyi", "vochysia guatemalensis")
+overstorypheno <- filter(month_treatment, species %in% overstory)
+
+ggplot(data = overstorypheno, aes(month, (log(seednum)+1), fill = species)) +
+  stat_summary(fun.y = sum, # adds up all observations for the month
+               geom = "bar")+ # or "line"
+  scale_x_date(date_breaks = "1 month", labels=date_format ("%b-%Y"), limits = as.Date(c(NA, NA)))+ # custom x-axis labels
+  facet_grid(treatment~.)+
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))+
+  scale_y_log10()
+
+#b) compare overstory phenology with phenologies of comprable dispersal strategy. #This code below does not do that.
+ggplot(overstorypheno, aes(month, (log(seednum)+1), color = species))+
+  stat_summary(fun.y = sum, geom = "line")+
+  scale_x_date(date_breaks = "1 month", labels=date_format ("%b-%Y"), limits = as.Date(c(NA, NA)))+ # custom x-axis labels
+  facet_grid(treatment~.)+
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+  
